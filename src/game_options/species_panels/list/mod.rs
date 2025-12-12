bcbb116c6124dbb2502ad58954833b406e202f80
Alexander Zagaynov <zalex80@gmail.com>
2025-12-12 02:27:31 +0100

    Refactor game_options/species_panels/list.rs into submodule

---
+++ src/game_options/species_panels/list/mod.rs
@@ -0,0 +1,6 @@
+mod item;
+mod panel;
+mod scroll;
+
+pub use item::spawn_species_list_item;
+pub use panel::spawn_species_list_panel;
--- src/game_options/species_panels/list/panel.rs
+++ src/game_options/species_panels/list/panel.rs
@@ -1,11 +1,12 @@
 use bevy::ecs::hierarchy::ChildSpawnerCommands;
 use bevy::prelude::*;
 
-use crate::data::{Language, NamedEntity, Species};
-use crate::game_options::types::{
-    ScrollButton, SpeciesListItem, SpeciesListScrollThumb, SpeciesListViewport,
-};
+use crate::data::Species;
+use crate::game_options::types::{ScrollButton, SpeciesListScrollThumb, SpeciesListViewport};
 use crate::game_options::ui::colors;
+
+use super::item::spawn_species_list_item;
+use super::scroll::spawn_scroll_button;
 
 /// Spawns the species list panel on the right side.
 pub fn spawn_species_list_panel(parent: &mut ChildSpawnerCommands, species: &[Species]) {
@@ -108,115 +109,5 @@ pub fn spawn_species_list_panel(parent: &mut ChildSpawnerCommands, species: &[Sp
                         ));
                     });
                 });
-        });
-}
-
-/// Spawns a scroll button (up or down).
-fn spawn_scroll_button(
-    col: &mut ChildSpawnerCommands,
-    icon: &str,
-    button_type: ScrollButton,
-    is_up: bool,
-) {
-    col.spawn((
-        Button,
-        Node {
-            width: Val::Percent(100.0),
-            height: Val::Px(24.0),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            margin: if is_up {
-                UiRect::bottom(Val::Px(5.0))
-            } else {
-                UiRect::top(Val::Px(5.0))
-            },
-            ..default()
-        },
-        BackgroundColor(colors::BUTTON_NORMAL),
-        button_type,
-    ))
-    .with_children(|btn| {
-        btn.spawn((
-            Text::new(icon),
-            TextFont {
-                font_size: 14.0,
-                ..default()
-            },
-            TextColor(colors::TEXT),
-        ));
-    });
-}
-
-/// Spawns a single species list item.
-pub fn spawn_species_list_item(
-    parent: &mut ChildSpawnerCommands,
-    index: usize,
-    species: &Species,
-    selected: bool,
-) {
-    let bg_color = if selected {
-        colors::SELECTED
-    } else {
-        colors::BUTTON_NORMAL
-    };
-
-    parent
-        .spawn((
-            Button,
-            Node {
-                width: Val::Percent(100.0),
-                height: Val::Px(80.0),
-                flex_direction: FlexDirection::Row,
-                align_items: AlignItems::Center,
-                padding: UiRect::all(Val::Px(8.0)),
-                margin: UiRect::bottom(Val::Px(5.0)),
-                border: UiRect::all(Val::Px(2.0)),
-                ..default()
-            },
-            BackgroundColor(bg_color),
-            BorderColor::all(if selected {
-                colors::TITLE
-            } else {
-                colors::PANEL_BORDER
-            }),
-            BorderRadius::all(Val::Px(8.0)),
-            SpeciesListItem { index },
-        ))
-        .with_children(|item| {
-            // Portrait circle
-            item.spawn((
-                Node {
-                    width: Val::Px(60.0),
-                    height: Val::Px(60.0),
-                    border: UiRect::all(Val::Px(2.0)),
-                    margin: UiRect::right(Val::Px(10.0)),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                BackgroundColor(colors::PANEL_BG),
-                BorderColor::all(colors::PANEL_BORDER),
-                BorderRadius::all(Val::Percent(50.0)),
-            ))
-            .with_children(|circle| {
-                circle.spawn((
-                    Text::new("👽"),
-                    TextFont {
-                        font_size: 28.0,
-                        ..default()
-                    },
-                    TextColor(colors::TEXT),
-                ));
-            });
-
-            // Species name
-            item.spawn((
-                Text::new(species.name(Language::En)),
-                TextFont {
-                    font_size: 14.0,
-                    ..default()
-                },
-                TextColor(colors::TEXT),
-            ));
         });
 }
